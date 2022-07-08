b05a7be8a87138c45e63a2ac783ee7824e3adf25
Gani Georgiev <gani.georgiev@gmail.com>
2022-07-08 13:15:01 +0300

    updated ui/dist

---
+++ ui/dist/assets/NotFoundPage.899a1d8c.js
@@ -0,0 +1,1 @@
+import{S as t,i as n,s as o,K as a}from"./index.a658f3b6.js";function l(e){return a("/collections"),[]}class c extends t{constructor(s){super(),n(this,s,l,null,o,{})}}export{c as default};
