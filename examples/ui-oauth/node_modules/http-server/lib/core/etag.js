f96609466b2caf62d4ac9810c4ac8d8f3027032d
Mats Becker <mats.becker@gmail.com>
2022-09-08 14:25:47 +0200

    add custom mail

--- examples/ui-oauth/node_modules/http-server/lib/core/etag.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = (stat, weakEtag) => {
-  let etag = `"${[stat.ino, stat.size, stat.mtime.toISOString()].join('-')}"`;
-  if (weakEtag) {
-    etag = `W/${etag}`;
-  }
-  return etag;
-};
