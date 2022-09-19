d9d8afcb27ca9c815fe51d14426d477b82ed085c
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2022-09-19 12:33:44 -0400

    hack to include client id into the header of the user api req

--- tools/auth/twitch.go
+++ tools/auth/twitch.go
@@ -1,6 +1,12 @@
 package auth
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
 	"golang.org/x/oauth2"
 )
 
@@ -37,7 +43,7 @@ func (p *Twitch) FetchAuthUser(token *oauth2.Token) (*AuthUser, error) {
 		} `json:"data"`
 	}{}
 
-	if err := p.FetchRawUserData(token, &rawData); err != nil {
+	if err := p.fetchUser(token, &rawData); err != nil {
 		return nil, err
 	}
 
@@ -50,4 +56,41 @@ func (p *Twitch) FetchAuthUser(token *oauth2.Token) (*AuthUser, error) {
 	}
 
 	return user, nil
+}
+
+// Temporary hack to get the user from the Twitch API.
+// Must be done this way so that we can include the "Client-ID" header.
+func (p *Twitch) fetchUser(token *oauth2.Token, result any) error {
+	req, err := http.NewRequest("GET", p.userApiUrl, nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token.AccessToken))
+	req.Header.Set("Client-Id", p.clientId)
+
+	client := &http.Client{Timeout: time.Second * 10}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	// http.Client.Get doesn't treat non 2xx responses as error
+	if resp.StatusCode >= 400 {
+		return fmt.Errorf(
+			"Failed to fetch OAuth2 user profile via %s (%d):\n%s",
+			p.userApiUrl,
+			resp.StatusCode,
+			string(content),
+		)
+	}
+
+	return json.Unmarshal(content, &result)
 }
