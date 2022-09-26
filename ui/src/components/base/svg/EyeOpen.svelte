f61c8dc9a65ea416541325133065dc1124461784
Tyler Sustare <10850753+TylerSustare@users.noreply.github.com>
2022-09-25 22:31:54 -0700

    Add SVGs as Svelte components for show/hide password

---
+++ ui/src/components/base/svg/EyeOpen.svelte
@@ -0,0 +1,19 @@
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke="currentColor"
+>
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+  />
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+  />
+</svg>
