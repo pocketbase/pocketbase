632e571f4b323d193a6f684a5902d817eae3b5e7
Lâm Hoàng Phúc <64113128+TcMits@users.noreply.github.com>
2022-12-16 11:29:43 +0700

    format: run linter

--- daos/base.go
+++ daos/base.go
@@ -83,18 +83,18 @@ func (dao *Dao) SyncDB() dbx.Builder {
 //
 // Example:
 //
-//		func someLongRunningTransaction() error {
-//			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-//			defer cancel()
-//			if err := app.Dao().Block(ctx); err != nil {
-//				return err
-//			}
-//			defer app.Dao().Continue()
+// 	func someLongRunningTransaction() error {
+// 		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+// 		defer cancel()
+// 		if err := app.Dao().Block(ctx); err != nil {
+// 			return err
+// 		}
+// 		defer app.Dao().Continue()
 //
-//	 	return app.Dao().RunInTransaction(func (txDao *daos.Dao) error {
-//	 	    // some long running read&write transaction...
-//	 	})
-//		}
+//  	return app.Dao().RunInTransaction(func (txDao *daos.Dao) error {
+//  	    // some long running read&write transaction...
+//  	})
+// 	}
 func (dao *Dao) Block(ctx context.Context) error {
 	if dao.sem == nil {
 		dao.mux.Lock()
