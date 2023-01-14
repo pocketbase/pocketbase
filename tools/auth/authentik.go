6d25d57f7da50a72fdad81642ca2f4b1a86c2a72
Marc Singer <ms@pr0.tech>
2023-01-14 11:58:55 +0100

    Finalized the oauth provider

--- tools/auth/authentik.go
+++ tools/auth/authentik.go
@@ -6,29 +6,76 @@ package auth
  */
 
 import (
-	"fmt"
+	"encoding/json"
+	"strconv"
 
 	"golang.org/x/oauth2"
 )
 
 var _ Provider = (*Authentik)(nil)
 
-// Name of the oauth provider
+// Unique name of this authentification provider
 const NameAuthentik string = "authentik"
 
+// Authentik is a self hosted authentification provider
 type Authentik struct {
 	*baseProvider
 }
 
+// Create a new Authentik provider instance with some defaults
 func NewAuthentikProvider() *Authentik {
 	return &Authentik{&baseProvider{
-		scopes:   []string{"profile"},
-		authUrl:  "",
-		tokenUrl: "",
+		// Default scopes provided to any oauth2 providers
+		scopes: []string{
+			"email",
+			"profile",
+			"openid",
+		},
+		authUrl:    "",
+		tokenUrl:   "",
+		userApiUrl: "",
 	}}
 }
 
+// FetchAuthUser returns an AuthUser instance based on the Authentik user api
+// Reference:
 func (p *Authentik) FetchAuthUser(token *oauth2.Token) (*AuthUser, error) {
-	// TODO: Implement auth user function
-	return nil, fmt.Errorf("not implemented")
+	data, err := p.FetchRawUserData(token)
+	if err != nil {
+		return nil, err
+	}
+
+	rawUser := map[string]any{}
+	if err := json.Unmarshal(data, &rawUser); err != nil {
+		return nil, err
+	}
+
+	extracted := struct {
+		Id       int    `json:"id"`
+		Name     string `json:"name"`
+		Username string `json:"preferred_username"`
+		Email    string `json:"email"`
+	}{}
+	if err := json.Unmarshal(data, &extracted); err != nil {
+		return nil, err
+	}
+
+	// Username value in authentik is not restricted with min / max length
+	if len(extracted.Username) <= 3 || len(extracted.Username) > 100 {
+		// Update username to be empty, and use only e-mail address
+		extracted.Username = ""
+	}
+
+	user := &AuthUser{
+		Id:           strconv.Itoa(extracted.Id),
+		Name:         extracted.Name,
+		Username:     extracted.Username,
+		Email:        extracted.Email,
+		RawUser:      rawUser,
+		AccessToken:  token.AccessToken,
+		RefreshToken: token.RefreshToken,
+	}
+
+	return user, nil
+
 }
