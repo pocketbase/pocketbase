dd8564e0ecfdcbad693741da89607e006bc92231
Ifiok Ekott <ekottifiok@gmail.com>
2023-02-14 12:18:27 +0100

    added a new line in line 10
    from this //go:embed all:dist to // go:embed all:dist
    caused this error when installing or running a file
    $GOPATH/pkg/mod/github.com/pocketbase/pocketbase@v0.12.3/ui/embed.go:10:12: pattern all:dist: no matching files found

--- ui/embed.go
+++ ui/embed.go
@@ -7,7 +7,7 @@ import (
 	"github.com/labstack/echo/v5"
 )
 
-//go:embed all:dist
+// go:embed all:dist
 var distDir embed.FS
 
 // DistDirFS contains the embedded dist directory files (without the "dist" prefix)
