56a77fd5ff54ac11ee143e8e2a36574e65c65cfb
Gani Georgiev <gani.georgiev@gmail.com>
2023-03-02 15:16:47 +0200

    updated migrations comment

--- migrations/logs/1677760279_uppsercase_method.go
+++ migrations/logs/1677760279_uppsercase_method.go
@@ -4,7 +4,7 @@ import (
 	"github.com/pocketbase/dbx"
 )
 
-// This migrations converts all logs "method" valus to all UPPERCASE (eg. "get" => "GET").
+// This migration normalizes the request logs method to UPPERCASE (eg. "get" => "GET").
 func init() {
 	LogsMigrations.Register(func(db dbx.Builder) error {
 		_, err := db.NewQuery("UPDATE {{_requests}} SET method=UPPER(method)").Execute()
