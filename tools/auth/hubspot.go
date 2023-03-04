e847a996d3fa793bbf29bd64ae38a5f5ea20beea
Julien Ernewein <ernewein.julien@gmail.com>
2023-03-04 18:36:50 +0100

    up

--- tools/auth/hubspot.go
+++ tools/auth/hubspot.go
@@ -3,7 +3,7 @@ package auth
 import (
 	"encoding/json"
 	"errors"
-	"time"
+	"strings"
 
 	"golang.org/x/oauth2"
 )
@@ -45,26 +45,12 @@ func (p *Hubspot) FetchAuthUser(token *oauth2.Token) (*AuthUser, error) {
 
 	var resp struct {
 		Results []struct {
-			ID        string    `json:"id"`
-			Email     string    `json:"email"`
-			FirstName string    `json:"firstName"`
-			LastName  string    `json:"lastName"`
-			UserID    int       `json:"userId"`
-			CreatedAt time.Time `json:"createdAt"`
-			UpdatedAt time.Time `json:"updatedAt"`
-			Archived  bool      `json:"archived"`
-			Teams     []struct {
-				ID      string `json:"id"`
-				Name    string `json:"name"`
-				Primary bool   `json:"primary"`
-			} `json:"teams"`
+			ID        string `json:"id"`
+			Email     string `json:"email"`
+			FirstName string `json:"firstName"`
+			LastName  string `json:"lastName"`
+			Archived  bool   `json:"archived"`
 		} `json:"results"`
-		Paging struct {
-			Next struct {
-				After string `json:"after"`
-				Link  string `json:"link"`
-			} `json:"next"`
-		} `json:"paging"`
 	}
 
 	if err := json.Unmarshal(data, &resp); err != nil {
@@ -84,7 +70,7 @@ func (p *Hubspot) FetchAuthUser(token *oauth2.Token) (*AuthUser, error) {
 	return &AuthUser{
 		Id:           profile.ID,
 		Email:        profile.Email,
-		Name:         profile.FirstName + " " + profile.LastName,
+		Name:         strings.Join([]string{profile.FirstName, profile.LastName}, " "),
 		RawUser:      rawUser,
 		AccessToken:  token.AccessToken,
 		RefreshToken: token.RefreshToken,
