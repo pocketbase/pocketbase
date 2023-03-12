cce9116fa792121b25ba8692abd9fa77f295c38c
Gani Georgiev <gani.georgiev@gmail.com>
2023-03-12 17:31:24 +0200

    updated ui/dist

--- ui/dist/assets/RealtimeApiDocs-1d12f17f.js
+++ ui/dist/assets/RealtimeApiDocs-1d12f17f.js
@@ -1,4 +1,4 @@
-import{S as re,i as ae,s as be,N as ue,C as P,e as u,w as y,b as a,c as te,f as p,g as t,h as I,m as ne,x as pe,t as ie,a as le,o as n,d as ce,R as me,p as de}from"./index-0b562d0f.js";import{S as fe}from"./SdkTabs-69545b17.js";function $e(o){var B,U,W,A,H,L,T,q,M,N,j,J;let i,m,l=o[0].name+"",b,d,h,f,_,$,k,c,S,v,w,R,C,g,E,r,D;return c=new fe({props:{js:`
+import{S as re,i as ae,s as be,N as ue,C as P,e as u,w as y,b as a,c as te,f as p,g as t,h as I,m as ne,x as pe,t as ie,a as le,o as n,d as ce,R as me,p as de}from"./index-4bd199fb.js";import{S as fe}from"./SdkTabs-fc1a80c5.js";function $e(o){var B,U,W,A,H,L,T,q,M,N,j,J;let i,m,l=o[0].name+"",b,d,h,f,_,$,k,c,S,v,w,R,C,g,E,r,D;return c=new fe({props:{js:`
         import PocketBase from 'pocketbase';
 
         const pb = new PocketBase('${o[1]}');
