b81112f82e17d1e00aef46bf7649db16fed879cb
Gani Georgiev <gani.georgiev@gmail.com>
2023-03-26 22:49:33 +0300

    minor ui adjustments

--- ui/src/components/collections/IndexUpsertPanel.svelte
+++ ui/src/components/collections/IndexUpsertPanel.svelte
@@ -16,7 +16,10 @@
     let codeEditorComponent;
     let isCodeEditorComponentLoading = false;
 
-    $: presetColumns = collection?.schema?.filter((f) => !f.toDelete)?.map((f) => f.name) || [];
+    $: presetColumns = (collection?.schema?.filter((f) => !f.toDelete)?.map((f) => f.name) || []).concat([
+        "created",
+        "updated",
+    ]);
 
     $: indexParts = CommonHelper.parseIndex(index);
 
