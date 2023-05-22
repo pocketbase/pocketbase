c370d8407446bff825d38ffd973daa9948d9af2a
Kunal Singh <kunalsin9h@gmail.com>
2023-05-22 10:18:22 +0530

    [#2527] removed unnecessary slice check

--- tools/list/list.go
+++ tools/list/list.go
@@ -27,9 +27,6 @@ func SubtractSlice[T comparable](base []T, subtract []T) []T {
 
 // ExistInSlice checks whether a comparable element exists in a slice of the same type.
 func ExistInSlice[T comparable](item T, list []T) bool {
-	if len(list) == 0 {
-		return false
-	}
 
 	for _, v := range list {
 		if v == item {
