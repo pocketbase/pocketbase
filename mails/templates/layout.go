bee8e0826a3ebf73180a483e2b349ff836dd2239
Gani Georgiev <gani.georgiev@gmail.com>
2024-04-05 20:33:56 +0300

    [#4707] added constrasting border to the default email template btn style

--- mails/templates/layout.go
+++ mails/templates/layout.go
@@ -54,6 +54,7 @@ const Layout = `
             display: inline-block;
             vertical-align: top;
             border: 0;
+            outline: 1px solid #e5e5e5;
             cursor: pointer;
             color: #fff !important;
             background: #16161a !important;
