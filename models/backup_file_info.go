86c357c4fb9d00f738862ddf56ae22b30f71f8f5
JonSolo4 <solojon913@gmail.com>
2024-07-12 16:10:21 -0700

    Delete models directory

--- models/backup_file_info.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "github.com/pocketbase/pocketbase/tools/types"
-
-type BackupFileInfo struct {
-	Key      string         `json:"key"`
-	Size     int64          `json:"size"`
-	Modified types.DateTime `json:"modified"`
-}
