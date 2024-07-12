04e16b17471b8efb2be8ee8247f218b341d046cf
JonSolo4 <solojon913@gmail.com>
2024-07-12 16:05:58 -0700

    Delete tools directory

--- tools/types/types.go
+++
@@ -1,8 +0,0 @@
-// Package types implements some commonly used db serializable types
-// like datetime, json, etc.
-package types
-
-// Pointer is a generic helper that returns val as *T.
-func Pointer[T any](val T) *T {
-	return &val
-}
