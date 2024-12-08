0fff45347767895c646bf5e221ccfc188713e755
夜風 <2479757568@qq.com>
2024-12-08 22:07:07 +0800

    Update i18n.js

--- ui/src/i18n.js
+++ ui/src/i18n.js
@@ -1,7 +1,13 @@
 import { register, init, getLocaleFromNavigator } from 'svelte-i18n';
 
-register('zh', () => import('./locales/zh.json'));
+register('de', () => import('./locales/de.json'));
 register('en', () => import('./locales/en.json'));
+register('es', () => import('./locales/es.json'));
+register('fr', () => import('./locales/fr.json'));
+register('ja', () => import('./locales/ja.json'));
+register('ko', () => import('./locales/ko.json'));
+register('ru', () => import('./locales/ru.json'));
+register('zh', () => import('./locales/zh.json'));
 
 await init({
   fallbackLocale: 'en',
