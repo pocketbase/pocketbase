ac3dc181be84039874f4d91d614690b34685ff2d
Naol-bm <profesornaoltena@gmail.com>
2024-12-16 20:49:45 +0300

    added some libsql staff

--- examples/base/main.go
+++ examples/base/main.go
@@ -2,131 +2,44 @@ package main
 
 import (
 	"log"
-	"net/http"
-	"os"
-	"path/filepath"
 	"strings"
 
-	"github.com/pocketbase/pocketbase"
-	"github.com/pocketbase/pocketbase/apis"
-	"github.com/pocketbase/pocketbase/core"
-	"github.com/pocketbase/pocketbase/plugins/ghupdate"
-	"github.com/pocketbase/pocketbase/plugins/jsvm"
-	"github.com/pocketbase/pocketbase/plugins/migratecmd"
-	"github.com/pocketbase/pocketbase/tools/hook"
+	"github.com/pocketbase/dbx"
+    "github.com/pocketbase/pocketbase"
+    "github.com/pocketbase/pocketbase/core"
+
+    _ "github.com/tursodatabase/libsql-client-go/libsql"
+
 )
 
+const (
+    token = "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MzQyODE1NzUsImlkIjoiZGUzYTVkNGYtZDNlOS00MTc5LWE5ZjYtZWM4MzE5YTljNTk3In0.hoKahQLjmaEJmoVANfUQ3e9uouft_GsMHDKz2pnYJMcPXCq2heDc2gFWbLHGXcPD0oCfXN3RXm8QHhv0zEYQCQ"// replace with your actual token
+    databaseURL = "libsql://pb-1st-turso-db-naol-bm.turso.io" // replace with your actual database url
+)
+
+
+func init() {
+    dbx.BuilderFuncMap["libsql"] = dbx.BuilderFuncMap["sqlite3"]
+}
+
+var enableLibSQL = false
+
 func main() {
-	app := pocketbase.New()
-
-	// ---------------------------------------------------------------
-	// Optional plugin flags:
-	// ---------------------------------------------------------------
-
-	var hooksDir string
-	app.RootCmd.PersistentFlags().StringVar(
-		&hooksDir,
-		"hooksDir",
-		"",
-		"the directory with the JS app hooks",
-	)
-
-	var hooksWatch bool
-	app.RootCmd.PersistentFlags().BoolVar(
-		&hooksWatch,
-		"hooksWatch",
-		true,
-		"auto restart the app on pb_hooks file change",
-	)
-
-	var hooksPool int
-	app.RootCmd.PersistentFlags().IntVar(
-		&hooksPool,
-		"hooksPool",
-		15,
-		"the total prewarm goja.Runtime instances for the JS app hooks execution",
-	)
-
-	var migrationsDir string
-	app.RootCmd.PersistentFlags().StringVar(
-		&migrationsDir,
-		"migrationsDir",
-		"",
-		"the directory with the user defined migrations",
-	)
-
-	var automigrate bool
-	app.RootCmd.PersistentFlags().BoolVar(
-		&automigrate,
-		"automigrate",
-		true,
-		"enable/disable auto migrations",
-	)
-
-	var publicDir string
-	app.RootCmd.PersistentFlags().StringVar(
-		&publicDir,
-		"publicDir",
-		defaultPublicDir(),
-		"the directory to serve static files",
-	)
-
-	var indexFallback bool
-	app.RootCmd.PersistentFlags().BoolVar(
-		&indexFallback,
-		"indexFallback",
-		true,
-		"fallback the request to index.html on missing static path (eg. when pretty urls are used with SPA)",
-	)
-
-	app.RootCmd.ParseFlags(os.Args[1:])
-
-	// ---------------------------------------------------------------
-	// Plugins and hooks:
-	// ---------------------------------------------------------------
-
-	// load jsvm (pb_hooks and pb_migrations)
-	jsvm.MustRegister(app, jsvm.Config{
-		MigrationsDir: migrationsDir,
-		HooksDir:      hooksDir,
-		HooksWatch:    hooksWatch,
-		HooksPoolSize: hooksPool,
-	})
-
-	// migrate command (with js templates)
-	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
-		TemplateLang: migratecmd.TemplateLangJS,
-		Automigrate:  automigrate,
-		Dir:          migrationsDir,
-	})
-
-	// GitHub selfupdate
-	ghupdate.MustRegister(app, app.RootCmd, ghupdate.Config{})
-
-	// static route to serves files from the provided public dir
-	// (if publicDir exists and the route path is not already defined)
-	app.OnServe().Bind(&hook.Handler[*core.ServeEvent]{
-		Func: func(e *core.ServeEvent) error {
-			if !e.Router.HasRoute(http.MethodGet, "/{path...}") {
-				e.Router.GET("/{path...}", apis.Static(os.DirFS(publicDir), indexFallback))
-			}
-
-			return e.Next()
-		},
-		Priority: 999, // execute as latest as possible to allow users to provide their own route
-	})
+	// config libsql
+   app := pocketbase.NewWithConfig(pocketbase.Config{
+        DBConnect: func(dbPath string) (*dbx.DB, error) {
+        	if(enableLibSQL){
+            	if strings.Contains(dbPath, "data.db") {
+                	return dbx.Open("libsql", databaseURL+"?authToken="+token)
+            	}
+        	}
+            // optionally for the logs (aka. pb_data/auxiliary.db) use the default local filesystem driver
+            return core.DefaultDBConnect(dbPath)
+        },
+        DefaultDataDir: "pb_custom_dir_name",
+    })
 
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
 	}
-}
-
-// the default pb_public dir location is relative to the executable
-func defaultPublicDir() string {
-	if strings.HasPrefix(os.Args[0], os.TempDir()) {
-		// most likely ran with go run
-		return "./pb_public"
-	}
-
-	return filepath.Join(os.Args[0], "../pb_public")
 }
